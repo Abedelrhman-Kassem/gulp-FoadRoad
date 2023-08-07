060d03bc2433f3cd9f9ca237ca779a8154e8dcbd
Abedelrhman-Kassem <107558049+Abedelrhman-Kassem@users.noreply.github.com>
2023-08-07 23:22:06 +0300

    finish resturant page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,10 @@
+// Set active link
+let title = document.title.split("/")[1];
+let links = document.querySelectorAll("header .nav-link");
+links.forEach(
+  (link) => link.innerHTML === title && link.classList.add("active")
+);
+
 // Start Service Page
 if (document.title == "FoadRoad/Service") {
   // Accordions Service Page
@@ -20,3 +27,49 @@ if (document.title == "FoadRoad/Service") {
   });
 }
 // End Service Page
+
+// Start Resturant
+
+if (document.title == "FoadRoad/Resturant") {
+  let input = document.querySelector(".resturant-find input");
+  let button = document.querySelector(".resturant-find button");
+  let cards = document.querySelectorAll(".resturant-resturants .card");
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    cards.forEach((card) => {
+      card.lastElementChild.firstElementChild.innerHTML
+        .toLowerCase()
+        .includes(input.value.toLowerCase().trim())
+        ? card.classList.remove("d-none")
+        : card.classList.add("d-none");
+    });
+  });
+  let filters = document.querySelectorAll(
+    ".resturant-resturants .filter .filter-result"
+  );
+
+  filters.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.classList.toggle("active");
+      filters.forEach((ele) => ele === item || ele.classList.remove("active"));
+
+      cards.forEach((card) => {
+        card.firstElementChild.lastElementChild.innerHTML
+          .toLowerCase()
+          .includes(item.innerHTML.toLowerCase().trim())
+          ? card.classList.remove("d-none")
+          : card.classList.add("d-none");
+      });
+      document.querySelector(".filter-result.active") == null &&
+        cards.forEach((card) => card.classList.remove("d-none"));
+    });
+  });
+}
+
+// End Resturant
+
+// Start Footer
+
+let copyRight = document.querySelector("footer .finals .copyright");
+copyRight.innerHTML = `Copyright © ${new Date().getFullYear()} FoodRoad Food delivery`;
